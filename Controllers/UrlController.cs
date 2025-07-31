0d7d0746de74a5752b5f15758dd5eecdd1ae8cb0
Carlossnogueira <carlosnogueira.tkdev@gmail.com>
2025-07-30 23:04:00 -0300

    feat: encode and decode id for short urls and save in database. Can visit your shorted url

--- Controllers/UrlController.cs
+++ Controllers/UrlController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using URL_Shortener.Data;
 using URL_Shortener.Model.Request;
+using URL_Shortener.Model.Url;
+using URL_Shortener.Util;
 
 namespace URL_Shortener.Controllers
 {
@@ -8,6 +10,11 @@ namespace URL_Shortener.Controllers
     {
 
         private readonly UrlContext _context;
+
+        public UrlController(UrlContext context)
+        {
+            _context = context;
+        }
 
         public IActionResult Index()
         {
@@ -18,19 +25,54 @@ namespace URL_Shortener.Controllers
         [Route("api/shorten")]
         public IActionResult Shorten([FromBody] UrlRequest request)
         {
-            Console.WriteLine(request.Url);
 
             if (string.IsNullOrEmpty(request.Url))
-            {
                 return BadRequest("URL cannot be empty.");
+
+            if (!request.Url.StartsWith("http://") && !request.Url.StartsWith("https://"))
+            {
+                request.Url = "http://" + request.Url;
             }
 
+            var existing = _context.Urls.FirstOrDefault(u => u.OriginalUrl == request.Url);
+            if (existing != null)
+            {
+                return Ok(new
+                {
+                    originalUrl = request.Url,
+                    shortUrl = $"http://localhost:5080/{existing.ShortUrl}"
+                });
+            }
+
+            var newUrl = new Url
+            {
+                OriginalUrl = request.Url
+            };
+
+            _context.Urls.Add(newUrl);
+            _context.SaveChanges();
+
+            newUrl.ShortUrl = CodeGenerator.Encode(newUrl.Id);
+            _context.SaveChanges();
 
             return Ok(new
             {
                 originalUrl = request.Url,
-                shortUrl = "test"
+                shortUrl = $"http://localhost:5080/{newUrl.ShortUrl}"
             });
+
+        }
+
+        [HttpGet]
+        [Route("{code}")]
+        public IActionResult GoToOriginalUrl(string code)
+        {
+            var id = CodeGenerator.Decode(code);
+            var link = _context.Urls.FirstOrDefault(l => l.Id == id);
+            if (link == null)
+                return NotFound("Url not founded");
+
+            return Redirect(link.OriginalUrl);
         }
     }
 }
---
+++ Util/CodeGenerator.cs
@@ -0,0 +1,34 @@
+﻿using System.Text;
+
+namespace URL_Shortener.Util
+{
+    public static class CodeGenerator
+    {
+        private const string _alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+        private static readonly int _base =  _alphabet.Length;
+
+        public static string Encode(int num)
+        {
+            var sb = new StringBuilder();
+            while (num > 0)
+            {
+                sb.Insert(0, _alphabet[num % _base]);
+                num /= _base;
+            }
+
+            return sb.ToString();
+        }
+
+        public static int Decode(string code)
+        {
+            int num = 0;
+            foreach (char c in code)
+            {
+                num = num * _base + _alphabet.IndexOf(c);
+            }
+
+            return num;
+        }
+
+    }
+}
